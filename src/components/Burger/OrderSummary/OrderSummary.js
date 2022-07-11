5ecce3545fbbcd5497482a4276b700c0ee035655
Realharpi <harpiwarrach@gmail.com>
2022-07-11 22:54:39 +0200

    Completed

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,37 @@
-import React from 'react';
+import React, { Component }from 'react';
 import Auxx from '../../../hoc/Auxx';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = ( props ) => {
-    const ingredientSummary = Object.keys( props.ingredMulti )
+class OrderSummary extends Component {
+
+    componentDidUpdate() {
+        console.log('[OrderSummary] DidUpdate')
+    }
+
+    render () {
+
+        const ingredientSummary = Object.keys( this.props.ingredMulti )
         .map( igKey => {
             return (
                 <li key={igKey}>
-                    <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredMulti[igKey]}
+                    <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingredMulti[igKey]}
                 </li> );
         } );
 
-    return (
-        <Auxx>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout!</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+        return (
+            <Auxx>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout!</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
         </Auxx>
-    );
-};
+        )
+    }
+}
 
-export default orderSummary;
+export default OrderSummary;
